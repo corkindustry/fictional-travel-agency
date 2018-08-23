3c3549d27bde9229c62e863c62bf3b9446e8f18a
jonah <falsefriend13@gmail.com>
2018-08-23 11:51:02 -0400

    added new packages and updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ watch = require('gulp-watch'),
 postcss = require('gulp-postcss'), 
 autoprefixer = require('autoprefixer'), 
 cssvars = require('postcss-simple-vars'), 
-nested = require('postcss-nested');
+nested = require('postcss-nested'), 
+cssimport = require('postcss-import');
 
 gulp.task('default', function() {
     console.log("You created a task!");
@@ -15,8 +16,9 @@ gulp.task('html', function() {
 
 gulp.task('styles', function() {
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe([cssvars, nested, autoprefixer])
+        .pipe(postcss([cssimport, cssvars, nested, autoprefixer]))
         .pipe(gulp.dest('./app/temp/styles'));
+        console.log("updated styles");
 });
 
 gulp.task('watch', function() {
